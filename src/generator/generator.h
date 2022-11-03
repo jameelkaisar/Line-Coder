d350099ee1baa7cc88a26ba0c2f4e48a9082f68b
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2022-11-03 19:13:47 +0530

    Add generator Header File

--- src/generator/generator.h
+++ src/generator/generator.h
@@ -6,15 +6,26 @@ class Generator {
    private:
    public:
     Generator() {
+        srand(time(0));
     }
 
     vector<int> getRandom(int n = 256) {
         vector<int> result;
+        for (int i = 0; i < n; i++) {
+            result.push_back(rand() % 2);
+        }
         return result;
     }
 
     vector<int> _getRandomWith(int n, int zeros, int x) {
         vector<int> result;
+        int j = rand() % (n - zeros);
+        for (int i = 0; i < n; i++) {
+            if (i >= j && i < j + zeros)
+                result.push_back(x);
+            else
+                result.push_back(rand() % 2);
+        }
         return result;
     }
 
