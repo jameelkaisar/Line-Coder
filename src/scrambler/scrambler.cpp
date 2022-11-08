aa2a2493c60d21c0191f9dd90effbfab30109f4e
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2022-11-08 18:52:27 +0530

    Fix scrambler.cpp

--- src/scrambler/scrambler.cpp
+++ src/scrambler/scrambler.cpp
@@ -2,7 +2,6 @@
 
 #include <bits/stdc++.h>
 
-#include "./../encoder/encoder.h"
 #include "./../generator/generator.h"
 #include "./../helper/helper.h"
 
@@ -10,14 +9,13 @@ using namespace std;
 
 int main() {
     Generator g;
-    Encoder e;
     Scrambler s;
 
     vector<int> vi = g.getRandomWithZeros(100, 16);
     cout << "vi -> " << to_stringv(vi, "") << endl;
 
-    cout << "HDB3 -> " << to_stringv(s.HDB3(e.NRZ_L(vi)), "") << endl;
-    cout << "B8ZS -> " << to_stringv(s.B8ZS(e.NRZ_L(vi)), "") << endl;
+    cout << "HDB3 -> " << to_stringv(s.HDB3(vi), "") << endl;
+    cout << "B8ZS -> " << to_stringv(s.B8ZS(vi), "") << endl;
 
     return 0;
 }
