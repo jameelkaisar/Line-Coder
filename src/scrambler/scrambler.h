5a21bce5b8b7691f9c46665594f8bbf059d8837a
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2022-11-08 18:21:40 +0530

    Add HDB3, B8ZS Scrambling

--- src/scrambler/scrambler.h
+++ src/scrambler/scrambler.h
@@ -10,11 +10,56 @@ class Scrambler {
 
     vector<int> HDB3(vector<int> bits) {
         vector<int> result;
+        int zc = 0;
+        bool flag = true;
+        for (auto x : bits) {
+            result.push_back(x);
+            if (x != 0) {
+                zc = 0;
+                flag = x > 0;
+            } else {
+                zc++;
+            }
+            if (zc == 8) {
+                int n = result.size();
+                result[n - 5] = flag ? 1 : -1;
+                result[n - 4] = flag ? -1 : 1;
+                result[n - 2] = flag ? -1 : 1;
+                result[n - 1] = flag ? 1 : -1;
+                zc = 0;
+            }
+        }
         return result;
     }
 
     vector<int> B8ZS(vector<int> bits) {
         vector<int> result;
+        int zc = 0;
+        bool flag = true;
+        bool prev = false;
+        for (auto x : bits) {
+            if (x != 0) {
+                result.push_back(prev ? -1 : 1);
+                zc = 0;
+                flag = !flag;
+                prev = !prev;
+            } else {
+                result.push_back(x);
+                zc++;
+            }
+            if (zc == 4) {
+                int n = result.size();
+                if (flag) {
+                    result[n - 1] = prev ? -1 : 1;
+                    result[n - 4] = prev ? -1 : 1;
+                } else {
+                    result[n - 1] = prev ? 1 : -1;
+                }
+                zc = 0;
+                flag = true;
+                prev = result[n - 1] > 0;
+            }
+        }
         return result;
     }
 };
