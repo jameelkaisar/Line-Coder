54a980c35663ab8cd5ce29d2f61750e20f9716e2
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2022-11-08 18:53:02 +0530

    Fix unscrambler.cpp

--- src/unscrambler/unscrambler.cpp
+++ src/unscrambler/unscrambler.cpp
@@ -2,8 +2,6 @@
 
 #include <bits/stdc++.h>
 
-#include "./../decoder/decoder.h"
-#include "./../encoder/encoder.h"
 #include "./../generator/generator.h"
 #include "./../helper/helper.h"
 #include "./../scrambler/scrambler.h"
@@ -12,16 +10,14 @@ using namespace std;
 
 int main() {
     Generator g;
-    Encoder e;
-    Decoder d;
     Scrambler s;
     Unscrambler u;
 
     vector<int> vi = g.getRandomWithZeros(100, 16);
     cout << "vi -> " << to_stringv(vi, "") << endl;
 
-    cout << "HDB3 -> " << to_stringv(d.NRZ_L(u.HDB3(s.HDB3(e.NRZ_L(vi)))), "") << endl;
-    cout << "B8ZS -> " << to_stringv(d.NRZ_L(u.B8ZS(s.B8ZS(e.NRZ_L(vi)))), "") << endl;
+    cout << "HDB3 -> " << to_stringv(u.HDB3(s.HDB3(vi)), "") << endl;
+    cout << "B8ZS -> " << to_stringv(u.B8ZS(s.B8ZS(vi)), "") << endl;
 
     return 0;
 }
